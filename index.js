43119cd1f621bc4eddbb9d3f8fb9bcb83d2b2c34
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-11 18:42:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var array = []
-  for (let i = 0; i < musicians.length; ) {
+  for (let i = 0; i < musicians.length; i++) {
     array.push('${musicans[i]} plays ${instruments[i]}')
   }
   return array
